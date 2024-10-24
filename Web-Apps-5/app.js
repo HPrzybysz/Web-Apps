5aad8d0e959a486c918c1180f9793b62eb2e23ee
HPrzybysz <niewica@gmail.com>
2024-10-24 09:32:39 +0200

    user messages are now stored in database

--- Web-Apps-5/app.js
+++ Web-Apps-5/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
 const app = express();
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
@@ -9,6 +10,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'contact_db',
+})
+
+db.connect(err => {
+    if (err){
+        console.error("Database connection error:", err)
+    }else{
+        console.log("Database connection success");
+    }
+})
 //Routes
 
 app.get('/', (req, res) => {
@@ -28,10 +43,53 @@ app.get('/kontakt', (req, res) => {
 });
 
 app.post('/kontakt', (req, res) => {
-    console.log(req.body);
-    res.redirect('/');
+
+    const { firstName, lastName, email, message } = req.body;
+
+    const query = 'INSERT INTO messages (firstName, lastName, email, message) VALUES (?,?,?,?)'
+
+    db.query(query, [firstName, lastName, email, message],
+        (err, result) => {
+        if (err){
+            console.error('Error while creating message', err);
+            res.status(500).send('Server error');
+        }else{
+            console.log('Successfully created message', result.insertId);
+            res.redirect('/')
+        }
+        })
+    // console.log(req.body);
+    //
+    // res.redirect('/');
 
 });
+
+app.get('/api/contact-messages', (req, res) => {
+    const query = 'SELECT * FROM messages';
+    db.query(query, (err, result) => {
+        if (err){
+            console.error('Error while retriving data', err);
+            res.status(500).send('Server error');
+        }else{
+            res.json(result);
+        }
+    })
+})
+
+app.get('/api/messages/:id', (req, res) => {
+    const id = req.params.id;
+    const query = 'SELECT * FROM messages WHERE id = ?';
+    db.query(query, [id], (err, result) => {
+        if (err){
+            console.error('Error while retriving data', err);
+            res.status(500).send('Server error');
+        }else if(result.length === 0){
+            res.status(404).send('No record found');
+        }else{
+            res.join(results[0]);
+        }
+    })
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
