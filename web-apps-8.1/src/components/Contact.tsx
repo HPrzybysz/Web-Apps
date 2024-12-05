8a04798cc1d950928c0ddbee83bb0397d7c951a9
HPrzybysz <niewica@gmail.com>
2024-12-05 09:33:17 +0100

    something

---
+++ web-apps-8.1/src/components/Contact.tsx
@@ -0,0 +1,12 @@
+// @ts-ignore
+import React from "react";
+const Contact: React.FC = () =>{
+    return(
+        <div>
+            <h1>Contact Page</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque distinctio dolore dolores ea esse et illum itaque laboriosam laudantium nemo officiis, praesentium quos soluta temporibus veniam voluptatem? Error, expedita!</p>
+        </div>
+    )
+}
+
+export default Contact;
